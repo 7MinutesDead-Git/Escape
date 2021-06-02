e7da216d2f4a875c463231c1341f417dcba660aa
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-06-02 13:24:35 -0500

    Add pressure plate functionality

--- Source/Escape/OpenDoor.cpp
+++ Source/Escape/OpenDoor.cpp
@@ -6,6 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "Math/UnrealMathUtility.h"
 
+// -------------------------------------------------------------------------------
 // Sets default values for this component's properties.
 UOpenDoor::UOpenDoor()
 {
@@ -15,24 +16,61 @@ UOpenDoor::UOpenDoor()
 	// ...
 }
 
+// -------------------------------------------------------------------------------
 // Called when the game starts.
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
+	Elapsed = 0;
+	DoorRotation = GetOwner()->GetActorRotation();
+	ClosedDoorPos = DoorRotation.Yaw;
+	OpenDoorPos = DoorRotation.Yaw + OpenAngle;
+	PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
 	// ...
 
+	if (!PressurePlate) {
+		FString CheckMe = GetOwner()->GetName();
+		UE_LOG(LogTemp, Error, TEXT("Pressure Plate hasn't been set for %s! Check OpenDoor component."), *CheckMe);
+		RequestEngineExit("No pressure plate set. See Error message.");
+	}
 }
 
 
+// -------------------------------------------------------------------------------
 // Called every frame.
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	// ----------------------------------------------------------------
+	// If we execute this without PressurePlate object being set in the editor, the engine will crash from null pointer.
+	if (PressurePlate->IsOverlappingActor(PlayerPawn)) {
+		Elapsed = 0;
+		OpenTheDoor(DeltaTime);
+	}
+	else {
+		// Start our door closing timer as soon as the player steps out of the trigger.
+		Elapsed += DeltaTime;
+		if (Elapsed > DoorOpenTime) {
+			CloseTheDoor(DeltaTime);
+		}
+	}
+}
 
-	AActor* Owner = GetOwner();
-	const FRotator DoorRotation = Owner->GetActorRotation();
+// -------------------------------------------------------------------------------
+/// Smoothly open the door towards TargetYawAdjustment.
+void UOpenDoor::OpenTheDoor(float DeltaTime)
+{
+	DoorRotation = GetOwner()->GetActorRotation();
 	// FInterpTo gives us a framerate-independent Lerp, by allowing us to pass in DeltaTime too.
-	SmoothedTargetRotation.Yaw = FMath::FInterpTo(DoorRotation.Yaw, TargetRotation.Yaw, DeltaTime, OpenRate);
-	Owner->SetActorRotation(SmoothedTargetRotation);
-	// ...
+	SmoothedTargetRotation.Yaw = FMath::FInterpTo(DoorRotation.Yaw, OpenDoorPos, DeltaTime, OpenRate);
+	GetOwner()->SetActorRotation(SmoothedTargetRotation);
+}
+
+// -------------------------------------------------------------------------------
+/// Smoothly close the door back towards StartingYaw position.
+void UOpenDoor::CloseTheDoor(float DeltaTime)
+{
+	DoorRotation = GetOwner()->GetActorRotation();
+	SmoothedTargetRotation.Yaw = FMath::FInterpTo(DoorRotation.Yaw, ClosedDoorPos, DeltaTime, OpenRate);
+	GetOwner()->SetActorRotation(SmoothedTargetRotation);
 }
--- Source/Escape/OpenDoor.h
+++ Source/Escape/OpenDoor.h
@@ -1,12 +1,11 @@
 // Alex Gulikers, 7 Minutes Dead, Copyright 2021
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "Math/Rotator.h"
 #include "OpenDoor.generated.h"
-
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
@@ -15,24 +14,36 @@ class ESCAPE_API UOpenDoor : public UActorComponent
 	GENERATED_BODY()
 
 public:
+	// -------------------------------------------------------------------------
 	// Sets default values for this component's properties
 	UOpenDoor();
 
 protected:
+	// -------------------------------------------------------------------------
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:
+	// -------------------------------------------------------------------------
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 private:
-	float TargetPitch = 0;
-	float TargetYaw = 180;
-	float TargetRoll = 0;
-	float OpenRate = 1;
-	FRotator TargetRotation{TargetPitch, TargetYaw, TargetRoll};
+	// -------------------------------------------------------------------------
+	void OpenTheDoor(float DeltaTime);
+	void CloseTheDoor(float DeltaTime);
+	float ClosedDoorPos;
+	float OpenDoorPos;
+
+	UPROPERTY(EditAnywhere) float OpenAngle = 90;
+
+	FRotator DoorRotation;
 	FRotator SmoothedTargetRotation;
 
+	UPROPERTY(EditAnywhere) float OpenRate = 1;
+	UPROPERTY(EditAnywhere) float DoorOpenTime = 1;
+	UPROPERTY(EditAnywhere) ATriggerVolume* PressurePlate;
+	UPROPERTY(EditAnywhere) APawn* PlayerPawn;
+	float Elapsed;
 
 };
